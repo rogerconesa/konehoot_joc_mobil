f47029759f2b99c6fe0be42c0967a3b7c377b7af
rogerconesa <43811804+rogerconesa@users.noreply.github.com>
2026-05-15 18:49:51 +0200

    Add files via upload

--- joc_mobil/sw.js
+++ joc_mobil/sw.js
@@ -2,13 +2,19 @@ const CACHE_NAME = 'konehoot-mobil-v1';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    return;
+  }
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
